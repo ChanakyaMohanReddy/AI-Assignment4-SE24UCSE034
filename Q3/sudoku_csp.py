37ed75593243309b6b0ee60620a7e6b76601e7cc
ChanakyaMohanReddy <chanakyamohanreddy@gmail.com>
2026-04-02 22:04:25 +0530

    Create sudoku_csp.py

---
+++ Q3/sudoku_csp.py
@@ -0,0 +1,44 @@
+grid = [
+    [0,0,3,0,2,0,6,0,0],
+    [9,0,0,3,0,5,0,0,1],
+    [0,0,1,8,0,6,4,0,0],
+    [0,0,8,1,0,2,9,0,0],
+    [7,0,0,0,0,0,0,0,8],
+    [0,0,6,7,0,8,2,0,0],
+    [0,0,2,6,0,9,5,0,0],
+    [8,0,0,2,0,3,0,0,9],
+    [0,0,5,0,1,0,3,0,0]
+]
+
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num or board[i][col] == num:
+            return False
+
+    start_row, start_col = 3*(row//3), 3*(col//3)
+
+    for i in range(3):
+        for j in range(3):
+            if board[start_row+i][start_col+j] == num:
+                return False
+
+    return True
+
+def solve(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                for num in range(1,10):
+                    if is_valid(board, i, j, num):
+                        board[i][j] = num
+                        if solve(board):
+                            return True
+                        board[i][j] = 0
+                return False
+    return True
+
+solve(grid)
+
+print("Sudoku Solution:\n")
+for row in grid:
+    print(*row)
